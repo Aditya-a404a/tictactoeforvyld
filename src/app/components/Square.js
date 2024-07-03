4dbdbd15c31f242f80ac361ae1b95aee3045077d
Aditya-a404a <aditya789arora@gmail.com>
2024-07-03 12:33:34 +0530

    Update Square.js

--- src/app/components/Square.js
+++ src/app/components/Square.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Square = ({ value, onClick, marginx ,marginy }) => {
   return (
-    <button className={  `text-custom-purp font-bold text-xl bg-custom-cyan hover:bg-custom-orange w-10  h-10 m-1`} id ="btn" onClick={onClick}>
+    <button className={  `text-custom-purp font-bold text-xl min-w-10 min-h-10 m-1 bg-custom-cyan hover:bg-custom-orange `} id ="btn" onClick={onClick}>
     
-      {value} 
+      {value? value : "_"} 
     </button>
   );
 };
